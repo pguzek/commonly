be6f5efcac91208a50611e7cd56368d3b21b10e9
Mateusz Pietrzak <mateusz.pietrzak@tellyo.com>
2019-09-02 16:36:19 +0200

    :construction_worker: Add a Travis CI to the project

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    moduleFileExtensions: [
+        "ts",
+        "tsx",
+        "js"
+    ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "<rootDir>/__build__/jest-transformer"
+    },
+    testMatch: [
+        "**/?(*.)spec.ts"
+    ],
+
+    moduleDirectories: [
+        "src", "node_modules"
+    ],
+
+    moduleNameMapper: {
+        "^commonly/(.*)$": "<rootDir>/src/$1"
+    },
+
+    collectCoverageFrom: [
+        "src/**/*.ts", "!src/Type/**/*.ts", "!src/**/index.ts"
+    ],
+
+    setupFiles: [
+        "jest-plugin-context/setup"
+    ]
+}
