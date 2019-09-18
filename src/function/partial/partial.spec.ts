e1acbf31f886b00c7c1e65fa0644627b242c8430
Mateusz Pietrzak <mateusz.pietrzak@tellyo.com>
2019-09-18 15:03:12 +0200

    Improve partial function typing

--- src/function/partial/partial.spec.ts
+++ src/function/partial/partial.spec.ts
@@ -71,6 +71,15 @@ describe("function partial(f, ...applied)", () => {
                         .toEqual(f(0, "1"))
                 })
             })
+
+            context("applied at 1st and 2nd position", () => {
+                const subject = partial(f, undefined, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, "1"))
+                        .toEqual(f(0, "1"))
+                })
+            })
         })
     })
 
@@ -128,6 +137,42 @@ describe("function partial(f, ...applied)", () => {
 
                 it("should return the same result as an f function", () => {
                     expect(subject(1))
+                        .toEqual(f(0, "1", 1))
+                })
+            })
+
+            context("applied at 1st and 2nd position", () => {
+                const subject = partial(f, undefined, undefined, 1)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, "1"))
+                        .toEqual(f(0, "1", 1))
+                })
+            })
+
+            context("applied at 1st and 3rd position", () => {
+                const subject = partial(f, undefined, "1", undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, 1))
+                        .toEqual(f(0, "1", 1))
+                })
+            })
+
+            context("applied at 2nd and 3rd position", () => {
+                const subject = partial(f, 0, undefined, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject("1", 1))
+                        .toEqual(f(0, "1", 1))
+                })
+            })
+
+            context("applied at 1st, 2nd and 3rd position", () => {
+                const subject = partial(f, undefined, undefined, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, "1", 1))
                         .toEqual(f(0, "1", 1))
                 })
             })
@@ -206,6 +251,105 @@ describe("function partial(f, ...applied)", () => {
 
                 it("should return the same result as an f function", () => {
                     expect(subject(2))
+                        .toEqual(f(0, "1", 1, 2))
+                })
+            })
+
+            context("applied at 1st and 2nd position", () => {
+                const subject = partial(f, undefined, undefined, 1, 2)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, "1"))
+                        .toEqual(f(0, "1", 1, 2))
+                })
+            })
+
+            context("applied at 1st and 3rd position", () => {
+                const subject = partial(f, undefined, "1", undefined, 2)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, 1))
+                        .toEqual(f(0, "1", 1, 2))
+                })
+            })
+
+            context("applied at 1st and 4th position", () => {
+                const subject = partial(f, undefined, "1", 1, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, 2))
+                        .toEqual(f(0, "1", 1, 2))
+                })
+            })
+
+            context("applied at 2nd and 3rd position", () => {
+                const subject = partial(f, 0, undefined, undefined, 2)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject("1", 1))
+                        .toEqual(f(0, "1", 1, 2))
+                })
+            })
+
+            context("applied at 2nd and 4th position", () => {
+                const subject = partial(f, 0, undefined, 1, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject("1", 2))
+                        .toEqual(f(0, "1", 1, 2))
+                })
+            })
+
+            context("applied at 3rd and 4th position", () => {
+                const subject = partial(f, 0, "1", undefined, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(1, 2))
+                        .toEqual(f(0, "1", 1, 2))
+                })
+            })
+
+            context("applied at 1st, 2nd and 3rd position", () => {
+                const subject = partial(f, undefined, undefined, undefined, 2)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, "1", 1))
+                        .toEqual(f(0, "1", 1, 2))
+                })
+            })
+
+            context("applied at 1st, 2nd and 4th position", () => {
+                const subject = partial(f, undefined, undefined, 1, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, "1", 2))
+                        .toEqual(f(0, "1", 1, 2))
+                })
+            })
+
+            context("applied at 1st, 3rd and 4th position", () => {
+                const subject = partial(f, undefined, "1", undefined, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, 1, 2))
+                        .toEqual(f(0, "1", 1, 2))
+                })
+            })
+
+            context("applied at 2nd, 3rd and 4th position", () => {
+                const subject = partial(f, 0, undefined, undefined, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject("1", 1, 2))
+                        .toEqual(f(0, "1", 1, 2))
+                })
+            })
+
+            context("applied at 1st, 2nd, 3rd and 4th position", () => {
+                const subject = partial(f, undefined, undefined, undefined, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, "1", 1, 2))
                         .toEqual(f(0, "1", 1, 2))
                 })
             })
@@ -302,6 +446,159 @@ describe("function partial(f, ...applied)", () => {
 
                 it("should return the same result as an f function", () => {
                     expect(subject(3))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 1st and 2nd position", () => {
+                const subject = partial(f, undefined, undefined, 1, 2, 3)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, "1"))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 1st and 3rd position", () => {
+                const subject = partial(f, undefined, "1", undefined, 2, 3)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, 1))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 1st and 4th position", () => {
+                const subject = partial(f, undefined, "1", 1, undefined, 3)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, 2))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 1st and 5th position", () => {
+                const subject = partial(f, undefined, "1", 1, 2, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, 3))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 2nd and 3rd position", () => {
+                const subject = partial(f, 0, undefined, undefined, 2, 3)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject("1", 1))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 2nd and 4th position", () => {
+                const subject = partial(f, 0, undefined, 1, undefined, 3)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject("1", 2))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 2nd and 5th position", () => {
+                const subject = partial(f, 0, undefined, 1, 2, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject("1", 3))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 3rd and 4th position", () => {
+                const subject = partial(f, 0, "1", undefined, undefined, 3)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(1, 2))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 3rd and 5th position", () => {
+                const subject = partial(f, 0, "1", undefined, 2, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(1, 3))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 4th and 5th position", () => {
+                const subject = partial(f, 0, "1", 1, undefined, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(2, 3))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 1st, 2nd and 3rd position", () => {
+                const subject = partial(f, undefined, undefined, undefined, 2, 3)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, "1", 1))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 1st, 2nd and 4th position", () => {
+                const subject = partial(f, undefined, undefined, 1, undefined, 3)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, "1", 2))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 1st, 2nd and 5th position", () => {
+                const subject = partial(f, undefined, undefined, 1, 2, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, "1", 3))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 1st, 3rd and 5th position", () => {
+                const subject = partial(f, undefined, "1", undefined, 2, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, 1, 3))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 1st, 4th and 5th position", () => {
+                const subject = partial(f, undefined, "1", 1, undefined, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(0, 2, 3))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 2nd, 4th and 5th position", () => {
+                const subject = partial(f, 0, undefined, 1, undefined, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject("1", 2, 3))
+                        .toEqual(f(0, "1", 1, 2, 3))
+                })
+            })
+
+            context("applied at 3rd, 4th and 5th position", () => {
+                const subject = partial(f, 0, "1", undefined, undefined, undefined)
+
+                it("should return the same result as an f function", () => {
+                    expect(subject(1, 2, 3))
                         .toEqual(f(0, "1", 1, 2, 3))
                 })
             })
--- src/type/PartialFunction.ts
+++ src/type/PartialFunction.ts
@@ -4,25 +4,46 @@ import VariadicFunction from "./VariadicFunction"
 
 
 type PartialFunction<TResult, TParameters extends unknown[], TApplied extends unknown[]> =
-    TApplied extends [ undefined, unknown, unknown, unknown, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<Shift<Reorder<TParameters, [ 1, 2, 3, 4, 0 ]>>>>>>
-        :
-    TApplied extends [ unknown, undefined, unknown, unknown, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<Shift<Reorder<TParameters, [ 0, 2, 3, 4, 1 ]>>>>>>
-        :
-    TApplied extends [ unknown, unknown, undefined, unknown, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<Shift<Reorder<TParameters, [ 0, 1, 3, 4, 2 ]>>>>>>
-        :
-    TApplied extends [ unknown, unknown, unknown, undefined, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<Shift<Reorder<TParameters, [ 0, 1, 2, 4, 3 ]>>>>>>
-        :
-    TApplied extends [ unknown, unknown, unknown, unknown, undefined ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<Shift<TParameters>>>>>
+    TApplied extends (unknown | undefined)[]
+        ? VariadicFunction<TResult, unknown[]>
         :
     TApplied extends [ unknown, unknown, unknown, unknown, unknown ]
         ? VariadicFunction<TResult, Shift<Shift<Shift<Shift<Shift<TParameters>>>>>>
         :
 
+    TApplied extends [ undefined, undefined, undefined, undefined ]
+        ? VariadicFunction<TResult, TParameters>
+        :
+    TApplied extends [ undefined, undefined, undefined, unknown ]
+        ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 3, 0, 1, 2, 4 ]>>>
+        :
+    TApplied extends [ undefined, undefined, unknown, undefined ]
+        ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 2, 0, 1, 3, 4 ]>>>
+        :
+    TApplied extends [ undefined, unknown, undefined, undefined ]
+        ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 1, 0, 2, 3, 4 ]>>>
+        :
+    TApplied extends [ unknown, undefined, undefined, undefined ]
+        ? VariadicFunction<TResult, Shift<TParameters>>
+        :
+    TApplied extends [ undefined, undefined, unknown, unknown ]
+        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 2, 3, 0, 1, 4 ]>>>>
+        :
+    TApplied extends [ undefined, unknown, undefined, unknown ]
+        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 1, 3, 0, 2, 4 ]>>>>
+        :
+    TApplied extends [ unknown, undefined, undefined, unknown ]
+        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 0, 3, 1, 2, 4 ]>>>>
+        :
+    TApplied extends [ undefined, unknown, unknown, undefined ]
+        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 1, 2, 0, 3, 4 ]>>>>
+        :
+    TApplied extends [ unknown, undefined, unknown, undefined ]
+        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 0, 2, 1, 3, 4 ]>>>>
+        :
+    TApplied extends [ unknown, unknown, undefined, undefined ]
+        ? VariadicFunction<TResult, Shift<Shift<TParameters>>>
+        :
     TApplied extends [ undefined, unknown, unknown, unknown ]
         ? VariadicFunction<TResult, Shift<Shift<Shift<Reorder<TParameters, [ 1, 2, 3, 0, 4 ]>>>>>
         :
@@ -39,6 +60,18 @@ type PartialFunction<TResult, TParameters extends unknown[], TApplied extends un
         ? VariadicFunction<TResult, Shift<Shift<Shift<Shift<TParameters>>>>>
         :
 
+    TApplied extends [ undefined, undefined, undefined ]
+        ? VariadicFunction<TResult, TParameters>
+        :
+    TApplied extends [ undefined, undefined, unknown ]
+        ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 2, 0, 1, 3, 4 ]>>>
+        :
+    TApplied extends [ undefined, unknown, undefined ]
+        ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 1, 0, 2, 3, 4 ]>>>
+        :
+    TApplied extends [ unknown, undefined, undefined ]
+        ? VariadicFunction<TResult, Shift<TParameters>>
+        :
     TApplied extends [ undefined, unknown, unknown ]
         ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 1, 2, 0, 3, 4 ]>>>>
         :
@@ -48,19 +81,18 @@ type PartialFunction<TResult, TParameters extends unknown[], TApplied extends un
     TApplied extends [ unknown, unknown, undefined ]
         ? VariadicFunction<TResult, Shift<Shift<TParameters>>>
         :
-
     TApplied extends [ unknown, unknown, unknown ]
         ? VariadicFunction<TResult, Shift<Shift<Shift<TParameters>>>>
         :
 
+    TApplied extends [ undefined, undefined ]
+        ? VariadicFunction<TResult, TParameters>
+        :
     TApplied extends [ unknown, undefined ]
         ? VariadicFunction<TResult, Shift<TParameters>>
         :
     TApplied extends [ undefined, unknown ]
         ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 1, 0, 2, 3, 4 ]>>>
-        :
-    TApplied extends [ undefined, undefined ]
-        ? VariadicFunction<TResult, TParameters>
         :
     TApplied extends [ unknown, unknown ]
         ? VariadicFunction<TResult, Shift<Shift<TParameters>>>
