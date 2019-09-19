8e642170d72b31fc3f7ebacb36bc4583ff2d30b0
Mateusz Pietrzak <mateusz.pietrzak@tellyo.com>
2019-09-19 13:44:54 +0200

    Update Tuple type

--- src/type/PartialFunction.ts
+++ src/type/PartialFunction.ts
@@ -1,4 +1,4 @@
-import { Reorder, Shift } from "./Tuple"
+import Tuple from "./Tuple"
 import VariadicFunction from "./VariadicFunction"
 
 
@@ -8,101 +8,101 @@ type PartialFunction<TResult, TParameters extends unknown[], TApplied extends un
         ? VariadicFunction<TResult, unknown[]>
         :
     TApplied extends [ unknown, unknown, unknown, unknown, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<Shift<Shift<TParameters>>>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Shift<Tuple.Shift<Tuple.Shift<TParameters>>>>>>
         :
 
     TApplied extends [ undefined, undefined, undefined, undefined ]
         ? VariadicFunction<TResult, TParameters>
         :
     TApplied extends [ undefined, undefined, undefined, unknown ]
-        ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 3, 0, 1, 2, 4 ]>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Reorder<TParameters, [ 3, 0, 1, 2, 4 ]>>>
         :
     TApplied extends [ undefined, undefined, unknown, undefined ]
-        ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 2, 0, 1, 3, 4 ]>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Reorder<TParameters, [ 2, 0, 1, 3, 4 ]>>>
         :
     TApplied extends [ undefined, unknown, undefined, undefined ]
-        ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 1, 0, 2, 3, 4 ]>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Reorder<TParameters, [ 1, 0, 2, 3, 4 ]>>>
         :
     TApplied extends [ unknown, undefined, undefined, undefined ]
-        ? VariadicFunction<TResult, Shift<TParameters>>
+        ? VariadicFunction<TResult, Tuple.Shift<TParameters>>
         :
     TApplied extends [ undefined, undefined, unknown, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 2, 3, 0, 1, 4 ]>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Reorder<TParameters, [ 2, 3, 0, 1, 4 ]>>>>
         :
     TApplied extends [ undefined, unknown, undefined, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 1, 3, 0, 2, 4 ]>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Reorder<TParameters, [ 1, 3, 0, 2, 4 ]>>>>
         :
     TApplied extends [ unknown, undefined, undefined, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 0, 3, 1, 2, 4 ]>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Reorder<TParameters, [ 0, 3, 1, 2, 4 ]>>>>
         :
     TApplied extends [ undefined, unknown, unknown, undefined ]
-        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 1, 2, 0, 3, 4 ]>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Reorder<TParameters, [ 1, 2, 0, 3, 4 ]>>>>
         :
     TApplied extends [ unknown, undefined, unknown, undefined ]
-        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 0, 2, 1, 3, 4 ]>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Reorder<TParameters, [ 0, 2, 1, 3, 4 ]>>>>
         :
     TApplied extends [ unknown, unknown, undefined, undefined ]
-        ? VariadicFunction<TResult, Shift<Shift<TParameters>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<TParameters>>>
         :
     TApplied extends [ undefined, unknown, unknown, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<Reorder<TParameters, [ 1, 2, 3, 0, 4 ]>>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Shift<Tuple.Reorder<TParameters, [ 1, 2, 3, 0, 4 ]>>>>>
         :
     TApplied extends [ unknown, undefined, unknown, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<Reorder<TParameters, [ 0, 2, 3, 1, 4 ]>>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Shift<Tuple.Reorder<TParameters, [ 0, 2, 3, 1, 4 ]>>>>>
         :
     TApplied extends [ unknown, unknown, undefined, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<Reorder<TParameters, [ 0, 1, 3, 2, 4 ]>>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Shift<Tuple.Reorder<TParameters, [ 0, 1, 3, 2, 4 ]>>>>>
         :
     TApplied extends [ unknown, unknown, unknown, undefined ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<TParameters>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Shift<TParameters>>>>
         :
     TApplied extends [ unknown, unknown, unknown, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<Shift<TParameters>>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Shift<Tuple.Shift<TParameters>>>>>
         :
 
     TApplied extends [ undefined, undefined, undefined ]
         ? VariadicFunction<TResult, TParameters>
         :
     TApplied extends [ undefined, undefined, unknown ]
-        ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 2, 0, 1, 3, 4 ]>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Reorder<TParameters, [ 2, 0, 1, 3, 4 ]>>>
         :
     TApplied extends [ undefined, unknown, undefined ]
-        ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 1, 0, 2, 3, 4 ]>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Reorder<TParameters, [ 1, 0, 2, 3, 4 ]>>>
         :
     TApplied extends [ unknown, undefined, undefined ]
-        ? VariadicFunction<TResult, Shift<TParameters>>
+        ? VariadicFunction<TResult, Tuple.Shift<TParameters>>
         :
     TApplied extends [ undefined, unknown, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 1, 2, 0, 3, 4 ]>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Reorder<TParameters, [ 1, 2, 0, 3, 4 ]>>>>
         :
     TApplied extends [ unknown, undefined, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Reorder<TParameters, [ 0, 2, 1, 3, 4 ]>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Reorder<TParameters, [ 0, 2, 1, 3, 4 ]>>>>
         :
     TApplied extends [ unknown, unknown, undefined ]
-        ? VariadicFunction<TResult, Shift<Shift<TParameters>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<TParameters>>>
         :
     TApplied extends [ unknown, unknown, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<Shift<TParameters>>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<Tuple.Shift<TParameters>>>>
         :
 
     TApplied extends [ undefined, undefined ]
         ? VariadicFunction<TResult, TParameters>
         :
     TApplied extends [ unknown, undefined ]
-        ? VariadicFunction<TResult, Shift<TParameters>>
+        ? VariadicFunction<TResult, Tuple.Shift<TParameters>>
         :
     TApplied extends [ undefined, unknown ]
-        ? VariadicFunction<TResult, Shift<Reorder<TParameters, [ 1, 0, 2, 3, 4 ]>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Reorder<TParameters, [ 1, 0, 2, 3, 4 ]>>>
         :
     TApplied extends [ unknown, unknown ]
-        ? VariadicFunction<TResult, Shift<Shift<TParameters>>>
+        ? VariadicFunction<TResult, Tuple.Shift<Tuple.Shift<TParameters>>>
         :
 
     TApplied extends [ undefined ]
         ? VariadicFunction<TResult, TParameters>
         :
     TApplied extends [ unknown ]
-        ? VariadicFunction<TResult, Shift<TParameters>>
+        ? VariadicFunction<TResult, Tuple.Shift<TParameters>>
         :
 
     TApplied extends []
