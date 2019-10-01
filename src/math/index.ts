d854de46df091c4ef7d09dea147db34d0d693827
Mateusz Pietrzak <mateusz.pietrzak@tellyo.com>
2019-10-01 15:23:43 +0200

    Update Math module

---
+++ src/math/index.ts
@@ -0,0 +1,42 @@
+import add from "./add/add"
+import ceil from "./ceil/ceil"
+import decrement from "./decrement/decrement"
+import divide from "./divide/divide"
+import floor from "./floor/floor"
+import increment from "./increment/increment"
+import maximum from "./maxiumum/maximum"
+import minimum from "./minimum/minimum"
+import multiply from "./multiply/multiply"
+import round from "./round/round"
+import subtract from "./subtract/subtract"
+
+
+
+export {
+    add,
+    ceil,
+    decrement,
+    divide,
+    floor,
+    increment,
+    maximum,
+    minimum,
+    multiply,
+    round,
+    subtract
+}
+
+
+export default {
+    add,
+    ceil,
+    decrement,
+    divide,
+    floor,
+    increment,
+    maximum,
+    minimum,
+    multiply,
+    round,
+    subtract
+}
